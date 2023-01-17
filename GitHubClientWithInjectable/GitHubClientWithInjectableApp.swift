0b421afca08ca09c02a7da06b65ed66a9b97094f
Franco Bellu <franco.bellu@gmail.com>
2023-01-17 01:37:44 +0100

    Initial Commit

---
+++ GitHubClientWithInjectable/GitHubClientWithInjectableApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientWithInjectableApp.swift
+//  GitHubClientWithInjectable
+//
+//  Created by Franco Bellu on 17/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientWithInjectableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
